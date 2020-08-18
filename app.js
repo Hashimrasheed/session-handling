ae80921b0ceb3a275386f95f696c6525c76f2f3b
Hashim Rasheed <hashimrasheed@pop-os.localdomain>
2020-08-18 11:05:56 +0530

    completed

--- app.js
+++ app.js
@@ -6,10 +6,9 @@ const session= require("express-session");
 const { nextTick } = require("process");
 
 const app= express();
-app.use(express.urlencoded({extended:false}))
-//const ejs= require("ejs");
-//app.use('/',express.static("views"));
-//app.set('views',path.join(__dirname,'views'));
+app.use(express.urlencoded({extended: false}))
+
+
 app.set("view engine ",'ejs');
 app.use(session ({
     name : 'sid',
@@ -19,14 +18,16 @@ app.use(session ({
 }));
 const username="Hashim Rasheed";
 const password= 123;
-const checkloggedin = (req,res,next)=>{
-    if(req.session.username){
+
+const checkloggedin = (req, res, next)=>{
+    if(req.session.username) 
+    {
         res.redirect('/home');
     }else{
         next();
     }gin
 }
-const protectHome= (req,res,next) =>{
+const protectHome= (req, res, next) => {
     if(req.session.username){
         next();
     }else{
@@ -35,13 +36,13 @@ const protectHome= (req,res,next) =>{
 
 }
 
-app.get('/login',checkloggedin,(req,res)=>{ 
+app.get('/login',checkloggedin,(req, res) => {  
      res.render('login.ejs');
 });
-app.get('/home',protectHome,(req,res)=>{ 
+app.get('/home',protectHome,(req, res) => { 
     res.render('home.ejs',{name : req.session.username});
 });
-app.post('/login',(req,res)=>{
+app.post('/login',(req, res) => {
     if(username==req.body.username&& password==req.body.password){
         req.session.username = req.body.username;
         res.redirect('/home');
@@ -49,7 +50,7 @@ app.post('/login',(req,res)=>{
         res.redirect('/login');
     }
 })
-app.post ("/logout",(req ,res)=>{
+app.post ("/logout",(req, res)=>{
     res.clearCookie("sid");
     req.session.destroy();
     res.redirect("/login")
